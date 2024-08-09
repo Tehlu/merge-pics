d545212dc19cae307f9176594aaa7722fca2a038
Beza <bezuks@gmail.com>
2024-08-09 15:25:49 -0400

    Added alpha value to rgba converted picture

--- main.py
+++ main.py
@@ -11,4 +11,12 @@ image_b = cv2.imread("Malaysia.jpg")
 print("Processed")
 # We'll change the image format first if there's an issue
 
+# Converting to rgba
+imagex_rgba = cv2.cvtColor(image_x, cv2.COLOR_BGR2BGRA)
+
+# Alpha channel with 50% Opacity (128/255)
+alpha_channel = np.ones(imagex_rgba.shape[:2], dtype=image_x.dtype)*128
+
+# Adding the alpha channel to the rgba file
+imagex_rgba[:, :, 3] = alpha_channel
 
